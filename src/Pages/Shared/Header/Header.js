e5e487352a7a3c0fdb572f1763d3c2d296a221fa
Iamzayed007 <zhr1720@gmail.com>
2021-11-10 16:03:09 +0600

    header and footer

---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { HashLink } from 'react-router-hash-link';
+
+
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            {/* const {user,logOut} = useAuth() */}
+
+            <Navbar sticky="top" collapseOnSelect expand="lg" bg="myRed"  >
+                <Container>
+                    <Navbar.Brand as={HashLink} to="/" className="fs-1"> <span className="text-white">Medi Care</span>  </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="mx-auto">
+                            <Nav.Link as={HashLink} to="/"><span className="text-white" >Home</span></Nav.Link>
+                            <Nav.Link as={HashLink} to="/#services"><span className="text-white" >Services</span></Nav.Link>
+                            <Nav.Link as={HashLink} to="/#doctors"><span className="text-white" >Doctor</span></Nav.Link>
+                            <Nav.Link as={HashLink} to="/#doctors"><span className="text-white" >Appointment</span></Nav.Link>
+                            <Nav.Link as={HashLink} to="/contact"><span className="text-white" >Contact</span></Nav.Link>
+
+                        </Nav>
+
+
+                        <Nav><button className="btn btn-danger">Sign out </button></Nav>
+                        <Nav>
+                            <Nav.Link as={HashLink} to="/login" >Login</Nav.Link>
+                            <Nav.Link as={HashLink} to="/register" className="header-button">Sign up </Nav.Link>
+                        </Nav>
+
+
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
+
+        </div>
+    );
+};
+
+export default Header;
